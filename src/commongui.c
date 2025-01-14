547bc72cf45fbcba75fdf4e684e1614793976158
Simon Long <simon@raspberrypi.com>
2025-01-14 09:23:07 +0000

    Add GNU_SOURCE to meson

--- src/commongui.c
+++ src/commongui.c
@@ -25,8 +25,6 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ============================================================================*/
 
-#define _GNU_SOURCE
-
 #include <glib/gi18n.h>
 #include <glib/gprintf.h>
 #include <pulse/pulseaudio.h>
